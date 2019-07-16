aba81d4cdf9726aebe47525eab852404047f2fe2
Oka <tonkotsu6218@gmail.com>
2019-07-16 15:20:53 +0900

    created

---
+++ src/tsx/index.tsx
@@ -0,0 +1,14 @@
+import '../index.html';
+
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      test
+    </div>
+  );
+}
+
+ReactDom.render(<App />, document.querySelector('#App'));
