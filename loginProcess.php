00f7969afae4b38ea92c72a8dd4f4f2c04480815
Oka <tonkotsu6218@gmail.com>
2019-08-04 18:32:06 +0900

    added try and catch sentence

--- loginProcess.php
+++ loginProcess.php
@@ -5,21 +5,27 @@
   $login_id = $_POST['email'];
   $password = $_POST['password'];
 
-  $dbh = new PDO(
-    'mysql:host=db;dbname=webproLastAssignmentdb',
-    'user',
-    'password'
-  );
+  try{
+    $dbh = new PDO(
+      'mysql:host=db;dbname=webproLastAssignmentdb',
+      'user',
+      'password'
+    );
 
-  $stmt = $dbh->prepare(
-    'select * from userInfo' .
-    ' where userInfo.name = ?'
-  );
-  $stmt->execute([$login_id]);
-  $row = $stmt->fetch(PDO::FETCH_ASSOC);
-  if ($password === $row['password']) {
-    $_SESSION['user'] = $login_id;
-    $_SESSION['userAuthority'] = $row['authority'];
+    $stmt = $dbh->prepare(
+      'select * from userInfo' .
+      ' where userInfo.name = ?'
+    );
+    $stmt->execute([$login_id]);
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($password === $row['password']) {
+      $_SESSION['user'] = $login_id;
+      $_SESSION['authority'] = $row['authority'];
+    }
+    header('Location: index.php');
+  } catch (PDOException $e) {
+    var_dump($e);
   }
-  header('Location: index.php');
+
+  
   
