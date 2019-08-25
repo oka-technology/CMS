523ceb551a68c14f6c1c994d1a471c599b87b45d
Oka <tonkotsu6218@gmail.com>
2019-08-26 00:22:02 +0900

    removed public folder

--- public/convertAuthority.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  function convertAuthority($authorityNum){
-    $authorityBinary = decbin((int) $authorityNum);
-    $authorityBinaryFilled = sprintf('%03d', $authorityBinary);
-    $authorityBinaryArray = str_split($authorityBinaryFilled);
-    return $authorityBinaryArray;
-  }
--- public/footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="footer">
-  <p><small>&copy; 2019 Kaoru Okazoe</small></p>
-</footer>
