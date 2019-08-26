5d00a8a014a758828843b6733c04017bf348759d
Oka <tonkotsu6218@gmail.com>
2019-08-26 16:15:50 +0900

    added header and footer

---
+++ src/tsx/components/Footer.tsx
@@ -0,0 +1,27 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+
+const wrapper = css`
+  align-items: center;
+  background-color: #333;
+  color: #fff;
+  display: flex;
+  flex-shrink: 0;
+  height: 5.0rem;
+  justify-content: center;
+`;
+
+const footerText = css`
+  font-size: 1.6rem;
+  margin: 0;
+`
+
+const Footer = (): JSX.Element => {
+  return(
+    <footer css={wrapper}>
+      <p css={footerText}><small>&copy; 2019 Kaoru Okazoe</small></p>
+    </footer>
+  );
+}
+
+export default Footer;
