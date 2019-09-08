90bddaa0d0e9a67401d8d7ad521a859d16833f1c
Oka <tonkotsu6218@gmail.com>
2019-09-08 18:37:58 +0900

    added convertAuthority php file

---
+++ src/api/convertAuthority.php
@@ -0,0 +1,8 @@
+<?php
+
+  function convertAuthority($authorityNum){
+    $authorityBinary = decbin((int) $authorityNum);
+    $authorityBinaryFilled = sprintf('%03d', $authorityBinary);
+    $authorityBinaryArray = str_split($authorityBinaryFilled);
+    return $authorityBinaryArray;
+  }
