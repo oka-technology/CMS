6b8c2d1069e263631e5e619296a6ce5386b3ddef
Oka <tonkotsu6218@gmail.com>
2019-09-08 21:53:40 +0900

    added convertAuthorityNumToString function

--- src/tsx/modules/convertAuthority.ts
+++ src/tsx/modules/convertAuthority.ts
@@ -7,9 +7,18 @@ export const convertAuthorityNumToObject = (authority: number): Authority => {
 };
 
 export const convertAuthorityObjectToString = ({ admin, editor, viewer }: Authority): string => {
-  const arr = [];
+  const arr: string[] = [];
   admin ? arr.push('管理者') : null;
   editor ? arr.push('編集者') : null;
   viewer ? arr.push('閲覧者') : null;
+  return arr.join(', ');
+};
+
+export const convertAuthorityNumToString = (authority: number): string => {
+  const authorityBinary: string = authority.toString(2);
+  const arr: string[] = [];
+  authorityBinary[2] === '1' ? arr.push('管理者') : null;
+  authorityBinary[1] === '1' ? arr.push('編集者') : null;
+  authorityBinary[0] === '1' ? arr.push('閲覧者') : null;
   return arr.join(', ');
 };
