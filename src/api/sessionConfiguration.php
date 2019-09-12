d4ee8173ab5b9bd5021650652a94369ece76ad47
Oka <tonkotsu6218@gmail.com>
2019-09-12 19:05:33 +0900

    changed sessionConfig

--- src/api/sessionConfiguration.php
+++ src/api/sessionConfiguration.php
@@ -1,5 +1,5 @@
 <?php
-  ini_set( 'session.gc_maxlifetime', 600 );
+  ini_set( 'session.gc_maxlifetime', 86400 );
   ini_set( 'session.gc_probability', 1 );
   ini_set( 'session.gc_divisor', 1 );
   session_save_path('/var/www/html/session'); 
