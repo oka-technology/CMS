79ad91390d1f7eef64c8efd5930c4d9d27f5c399
Oka <tonkotsu6218@gmail.com>
2019-09-16 00:23:36 +0900

    added new category registration page

---
+++ src/tsx/components/LoggedIn/Main/NewCategoryRegistration/NewCategoryRegistration.tsx
@@ -0,0 +1,88 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+import { useState, Fragment, useEffect } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+
+import Title from '../../../../template/Title';
+import TextInput from '../../../../template/TextInput';
+import Button from '../../../../template/Button';
+import ErrorMessage from '../../../../template/ErrorMessage';
+import { TOP_PAGE_PATH, newCategoryRegistrationPage } from '../../../../data/pages';
+
+const formStyle = css`
+  & > *:first-child /* emotion-disable-server-rendering-unsafe-selector-warning-please-do-not-use-this-the-warning-exists-for-a-reason */ {
+    margin-top: 0;
+  }
+`;
+
+const labelStyle = css`
+  display: block;
+  font-size: 1.8rem;
+  margin-top: 3rem;
+`;
+
+type ResultsOfAddUserApi = {
+  successful: boolean;
+};
+
+type AddUserProps = {
+  permission: Permission;
+};
+
+const NewCategoryRegistration = ({ permission }: AddUserProps): JSX.Element => {
+  const [categoryName, setCatregoryName] = useState<string>('');
+  const [unsuccessful, setUnsuccessful] = useState<boolean>(false);
+
+  useEffect(() => {
+    document.title = newCategoryRegistrationPage.pageName;
+  }, []);
+
+  const onSetCategoryName = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setCatregoryName(e.target.value);
+  };
+
+  const registerCategoryToDB = (e: React.MouseEvent<HTMLElement>): void => {
+    e.preventDefault();
+    setUnsuccessful(false);
+    const params = {
+      categoryName: categoryName,
+    };
+    axios
+      .post('./api/registerCategory.php', params)
+      .then(({ data }) => {
+        const resultData: ResultsOfAddUserApi = data;
+        if (!resultData.successful) {
+          setUnsuccessful(true);
+        } else {
+          setCatregoryName('');
+        }
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.log(error);
+      });
+  };
+
+  return (
+    <Fragment>
+      <Title value={newCategoryRegistrationPage.pageName} />
+      <form css={formStyle}>
+        <label css={labelStyle}>
+          Title
+          <TextInput type="text" placeholder="" value={categoryName} onChange={onSetCategoryName} marginTop="0.5rem" />
+        </label>
+        <Button
+          as="submit"
+          value="Register"
+          onClick={registerCategoryToDB}
+          additionalStyle={{ backgroundColor: '#0528c2', marginTop: '4rem' }}
+        />
+      </form>
+      {unsuccessful ? <ErrorMessage value="You must fill in all of the fields." /> : null}
+      {permission.editor ? null : <Redirect to={TOP_PAGE_PATH} />}
+    </Fragment>
+  );
+};
+
+export default NewCategoryRegistration;
