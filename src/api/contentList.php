62a60730c38be8b911f1c8b60b4f0df3bb11f1d9
Oka <tonkotsu6218@gmail.com>
2019-09-16 00:20:25 +0900

    create show content list api

---
+++ src/api/contentList.php
@@ -0,0 +1,42 @@
+<?php
+  session_save_path('/var/www/html/session'); 
+  require_once('convertPermission.php');
+  session_start();
+  
+  $permission = $_SESSION['permission'];
+  if (convertPermission($permission)[0] != 1 && convertPermission($permission)[1] != 1) {
+    echo('no permission');
+    exit();
+  } else {
+    try{
+      $dbh = new PDO(
+        'mysql:host=db;dbname=webproLastAssignmentdb',
+        'user',
+        'password'
+      );
+
+      $stmt = $dbh->prepare(
+        'SELECT contents.id, contents.title, contents.registrationDate, categories.name as categoryName from contents join categories on contents.category_id = categories.id;'
+      );
+      $stmt->execute();
+
+      function counter() {
+        $num = 0;
+        return function() use (&$num){
+          return $num++;
+        };
+      };
+      $counter = counter();
+
+      while($rowOfContent = $stmt->fetch(PDO::FETCH_ASSOC)){
+        $index = $counter();
+        $result[$index]['id'] = $rowOfUserInfo['id'];
+        $result[$index]['category'] = $rowOfUserInfo['categoryName'];
+        $result[$index]['title'] = $rowOfUserInfo['title'];
+        $result[$index]['registrationDate'] = $rowOfUserInfo['registrationDate'];
+      }
+      echo(json_encode($result, JSON_PRETTY_PRINT));
+    } catch (PDOException $e) {
+      echo($e);
+    }
+  }
